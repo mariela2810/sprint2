a04d6ab076e5e3724268437636cb48fb6bd9910a
Mariela <nushita2014@gmail.com>
2024-11-21 12:29:47 -0300

    Add files via upload

---
+++ configuracion/conexion.mjs
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+    try{
+        await mongoose.connect(
+        'mongodb+srv://Grupo-03:grupo03@cursadanodejs.ls9ii.mongodb.net/Node-js',
+        {useNewUrlParser:true, useUnifiedTopology:true})
+        console.log('Conexion exitosa a Mongo DB')
+    }
+    catch(error){
+        console.error('Error al conectar a Mongo DB', error)  
+        process.exit(1)
+    }
+}

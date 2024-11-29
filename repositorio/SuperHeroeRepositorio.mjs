323e872e8a657df333bc6fb8f2bd83411430665c
Mariela <nushita2014@gmail.com>
2024-11-29 11:40:15 -0300

    Add files via upload

--- repositorio/SuperHeroeRepositorio.mjs
+++ repositorio/SuperHeroeRepositorio.mjs
@@ -1,20 +1,27 @@
 import SuperHeroe from "../modelo/SuperHeroe.mjs";
 import IRepositorio from "./IRepositorio.mjs";
 
+
 class SuperHeroeRepositorio extends IRepositorio{
-    async obtenerPorId(id){
-        return await SuperHeroe.findById(id)
+
+    async obtenerMayoresDe30(){
+
+      return await SuperHeroe.find({edad:{$gt:30}, planetaOrigen:'Tierra',
+                                    $expr:{$gte:[{$size:"$poderes"}, 2]}
+                                  })
     }
+
+    async obtenerPorId(id){
+        return await SuperHeroe.findOne({id:id})
+    }
+
     async obtenerTodos(){
         return await SuperHeroe.find({})
     }
+
     async buscarPorAtributo(atributo,valor){
         const consulta = {[atributo]:new RegExp(valor,'i')}
         return await SuperHeroe.find(consulta)
-    }
-    async obtenerMayoresDe30(){
-        return await SuperHeroe.find({edad:{$gt30}, planetaOrigen:'Tierra',
-                     poderes:{$size:{$gte:2}}})
     }
 }
 

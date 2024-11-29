47811763a3049462c6685612417b99b45605e30a
Mariela <nushita2014@gmail.com>
2024-11-29 11:41:55 -0300

    Add files via upload

--- ruta/superheroesRuta.mjs
+++ ruta/superheroesRuta.mjs
@@ -7,9 +7,13 @@ import { obtenerSuperheroePorIdControlador,
 
  const ruta = express.Router()
 
+
+
  ruta.get('/heroes', obtenerTodosLosSuperheroesControlador)
- ruta.get('/heroes/id:', obtenerSuperheroePorIdControlador)
+ ruta.get('/heroes/:id', obtenerSuperheroePorIdControlador)
  ruta.get('/heroes/buscar/:atributo/:valor', buscarSuperheroePorAtributoControlador)
- ruta.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controlador)
+ ruta.get('/heroes/edad/mayores-30', obtenerSuperheroesMayoresDe30Controlador)
+
+ console.log(`la ruta obtenida es. ${ruta}`)
 
  export default ruta
